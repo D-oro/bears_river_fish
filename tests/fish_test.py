bd3ad117ecaf98ddb8147f0ce5bd1c69354151a4
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-07-14 11:12:32 +0100

    changed fish.test.py, we don't need a self.fish function because we only run one test

--- tests/fish_test.py
+++ tests/fish_test.py
@@ -3,9 +3,9 @@ from src.fish import Fish
 
 class TestFish(unittest.TestCase):
 
-    def setUp(self):
-        self.fish = Fish("Florence")
+    # def setUp(self):
+    #     self.fish = Fish("Florence")
 
     def test_fish_has_name(self):
-        actual = self.fish.name
-        self.assertEqual("Florence", actual)
+        fish = Fish("Nemo")
+        self.assertEqual("Nemo", fish.name)
